fa1711604e660de62a76cc65d86c9859753ea154
osso <fdantealvarezt@gmail.com>
2022-11-19 21:01:33 -0500

    servicios, restaurants y products

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const api = express.Router();
 
 api.use("/auth", require("./auth"));
+api.use("/search", require("./search"));
+api.use("/products", require("./products"));
 api.use("/restaurants", require("./restaurants"));
-
 api.use("/user/restaurant", require("./userRestaurant"));
 
 module.exports = api;
